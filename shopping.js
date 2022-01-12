a1fb1b2a5d032f2b566768fc24965105a7a22720
Hilary Shaner <hilaryshaner@gmail.com>
2022-01-12 10:17:35 -0600

    added three items to shopping list

---
+++ shopping.js
@@ -0,0 +1,22 @@
+const items = [
+    {
+        id: 1,
+        food: "milk",
+        price: 2.99,
+        refrigerated: true
+    },
+    {
+        id: 2,
+        food: "eggs",
+        price: 4.99,
+        refrigerated: true
+    },
+    {
+        id: 3,
+        food: "peanut butter",
+        price: 1.99,
+        refrigerated: false
+    }
+]
+
+console.log(items)
